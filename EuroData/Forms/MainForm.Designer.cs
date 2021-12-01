f85c6667bedb95f84ae6e3bb173a94fc535ca75f
Purmann <p.purmann@outlook.de>
2021-12-01 20:39:17 +0100

    use MySQL.Data instead of MySqlConnector

--- EuroData/Forms/MainForm.Designer.cs
+++ EuroData/Forms/MainForm.Designer.cs
@@ -29,17 +29,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.debugBox = new System.Windows.Forms.TextBox();
+            this.debugOutputLabel = new System.Windows.Forms.Label();
+            this.debugButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // debugBox
+            // 
+            this.debugBox.Location = new System.Drawing.Point(267, 43);
+            this.debugBox.Multiline = true;
+            this.debugBox.Name = "debugBox";
+            this.debugBox.ReadOnly = true;
+            this.debugBox.Size = new System.Drawing.Size(232, 115);
+            this.debugBox.TabIndex = 0;
+            // 
+            // debugOutputLabel
+            // 
+            this.debugOutputLabel.Location = new System.Drawing.Point(154, 50);
+            this.debugOutputLabel.Name = "debugOutputLabel";
+            this.debugOutputLabel.Size = new System.Drawing.Size(107, 23);
+            this.debugOutputLabel.TabIndex = 1;
+            this.debugOutputLabel.Text = "Debug Output:";
+            // 
+            // debugButton
+            // 
+            this.debugButton.Location = new System.Drawing.Point(45, 43);
+            this.debugButton.Name = "debugButton";
+            this.debugButton.Size = new System.Drawing.Size(83, 30);
+            this.debugButton.TabIndex = 2;
+            this.debugButton.Text = "Debug";
+            this.debugButton.UseVisualStyleBackColor = true;
+            this.debugButton.Click += new System.EventHandler(this.debugButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.debugButton);
+            this.Controls.Add(this.debugOutputLabel);
+            this.Controls.Add(this.debugBox);
             this.Name = "MainForm";
             this.Text = "Main";
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.TextBox debugBox;
+        private System.Windows.Forms.Label debugOutputLabel;
+        private System.Windows.Forms.Button debugButton;
 
         #endregion
     }
--- EuroData/Forms/MainForm.cs
+++ EuroData/Forms/MainForm.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace EuroData.Forms
 {
     public partial class MainForm : Form
     {
+        // ToDo: make it configurable in a separate file (.env?)
+        private string connectionString = "server=localhost;uid=root;" +
+                                          "password=root;database=db_euro_data";
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void debugButton_Click(object sender, EventArgs e)
+        {
+            MySqlConnection dbCon = new MySqlConnection(connectionString);
+            try
+            {
+                debugBox.Text = "Connecting to database...";
+                dbCon.Open();
+                debugBox.Text = "Connected to database!";
+                dbCon.Close();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unknown Error"+ex.Message);
+            }
         }
     }
 }
