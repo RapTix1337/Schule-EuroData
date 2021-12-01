9255bd30d43a70ea03e896c58841620b5ca73c45
Purmann <p.purmann@outlook.de>
2021-12-01 21:16:10 +0100

    added debug data

--- EuroData/Forms/MainForm.cs
+++ EuroData/Forms/MainForm.cs
@@ -16,6 +16,8 @@ namespace EuroData.Forms
         // ToDo: make it configurable in a separate file (.env?)
         private string connectionString = "server=localhost;uid=root;" +
                                           "password=root;database=db_euro_data";
+        private MySqlCommand dbCommand;
+        private MySqlConnection dbCon;
         public MainForm()
         {
             InitializeComponent();
@@ -23,12 +25,27 @@ namespace EuroData.Forms
 
         private void debugButton_Click(object sender, EventArgs e)
         {
-            MySqlConnection dbCon = new MySqlConnection(connectionString);
+            dbCon = new MySqlConnection(connectionString);
             try
             {
                 debugBox.Text = "Connecting to database...";
                 dbCon.Open();
                 debugBox.Text = "Connected to database!";
+
+                dbCommand = dbCon.CreateCommand();
+                dbCommand.CommandType = CommandType.Text;
+                dbCommand.CommandText = $"SELECT * FROM test";
+                MySqlDataReader reader = dbCommand.ExecuteReader();
+                if (reader.HasRows) {
+                    int count = reader.FieldCount;
+                    debugBox.Text += $"\r\nFound {count.ToString()} row(s):";
+                    while (reader.Read())
+                    {
+                        debugBox.Text += "\r\n"+reader.GetString("test_text");
+                    }
+                    reader.Close();
+                }
+                
                 dbCon.Close();
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,47 @@
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 
--- Insert SQL code
+--
+-- Datenbank: `db_euro_data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `test`
+--
+
+CREATE TABLE `test` (
+                        `id` int NOT NULL,
+                        `test_text` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Daten für Tabelle `test`
+--
+
+INSERT INTO `test` (`id`, `test_text`) VALUES
+                                           (1, 'text1'),
+                                           (2, 'text2');
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `test`
+--
+ALTER TABLE `test`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `test`
+--
+ALTER TABLE `test`
+    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 COMMIT;
