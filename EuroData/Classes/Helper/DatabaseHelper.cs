5410e82b0f8f7f76c2049992d69ebc95fda21f95
Purmann <p.purmann@outlook.de>
2021-12-02 18:53:10 +0100

    added helper for database

---
+++ EuroData/Classes/Helper/DatabaseHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace EuroData.Classes.Helper
+{
+    public class DatabaseHelper
+    {
+        private string connectionString = "server=localhost;uid=root;" +
+                                          "password=root;database=db_euro_data";
+
+        private MySqlConnection dbCon;
+        private MySqlCommand dbCommand;
+        
+        public DatabaseHelper(string connectionString = null)
+        {
+            if (connectionString != null)
+            {
+                this.connectionString = connectionString;
+            }
+            dbCon = new MySqlConnection(connectionString);
+            dbCommand = dbCon.CreateCommand();
+        }
+    }
+}
